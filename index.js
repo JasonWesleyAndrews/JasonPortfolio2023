fce302372240b875dabc419ee1b10ed093210097
JasonWesleyAndrews <85419439+JasonWesleyAndrews@users.noreply.github.com>
2023-08-05 18:10:35 -0400

    Init Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+
+
+// mobile nav show/hide //
+
+$("#mobileNav").on("click", function() {
+    if ( $(".subNav").first().is( ":hidden" ) ) {
+      $(".subNav").slideDown("medium");
+      $(".grayBG").fadeIn("medium");
+      $('html, body').css({
+        overflow: 'hidden',
+    })
+      document.getElementById("mobileNav").innerHTML = "X";
+      
+    } else {
+      $(".subNav").slideUp("medium");
+      $(".grayBG").fadeOut("medium");
+      $('html, body').css({
+        overflow: 'auto',  
+    })
+      document.getElementById("mobileNav").innerHTML = "Menu";
+    }
+  });
+
+  function myFunction(x) {
+    if (x.matches) {
+      $(".subNav").slideUp("medium");
+      $(".grayBG").fadeOut("medium");
+      
+      document.getElementById("mobileNav").innerHTML = "Menu";
+    } 
+  }
+  var x = window.matchMedia("(min-width: 600px)")
+  myFunction(x)
+  x.addListener(myFunction)
+
+  //header hide on scroll //
+
+    var prevScrollPos = window.pageYOffset;
+    var header = document.getElementById("header");
+
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+      if (prevScrollPos > currentScrollPos) {
+        header.style.top = "0";
+      } else {
+        header.style.top = "-65px";
+      }
+      prevScrollPos = currentScrollPos;
+    };
+  
